e4961558500da368c2a7007f11784544d093928a
LD <LDinho@users.noreply.github.com>
2019-06-07 22:38:03 -0400

    add user route get endpoint

---
+++ api/routes/users/usersRouter.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const router = express.Router();
+
+const {
+  getUsers,
+
+} = require('../../helpers/usersDbhelper');
+
+/*
+@GET: users
+@PARAMS: none
+@ROUTE: "/api/users"
+*/
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await getUsers();
+
+    if (users.length) {
+      res.status(200).json(users)
+
+    } else {
+      res.status(404).json({message: `No users found`})
+
+    }
+  }
+  catch (err) {
+    res.status(500).json({error: `Unable to retrieve users`})
+  }
+
+});
+
+module.exports = router;
